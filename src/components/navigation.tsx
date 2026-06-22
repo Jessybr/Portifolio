eede1354ec5515d5202c73f9ddc52702e5202ea4
Jessybr <jessicabuenoramos5@gmail.com>
2026-06-22 20:42:11 -0300

    feat: create Head component

---
+++ src/components/navigation.tsx
@@ -0,0 +1,19 @@
+//estilização do header
+import '../style/style.css'
+
+function Head() {
+    return (
+        <>
+        <nav>
+            <div className="cont_naveg">
+                <h3><a href="#habili">Habilidades</a></h3>
+                <h3><a href="#proje">Projetos</a></h3>
+                <h3><a href="#sobre">Sobre</a></h3>
+                <h3><a href="#contat">Contato</a></h3>
+            </div>
+        </nav>
+        </>
+    )
+}
+
+export default Head
