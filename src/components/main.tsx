20afa025e41972d941eae070b9f8c2dd961302cd
Jessybr <jessicabuenoramos5@gmail.com>
2026-06-22 21:47:17 -0300

    feat: create main component and add skills and projects components

---
+++ src/components/main.tsx
@@ -0,0 +1,13 @@
+import Projects from "./projects"
+import Skills from "./skills"
+
+function Main() {
+    return (
+        <main>
+            <Skills />
+            <Projects />
+        </main>
+    )
+}
+
+export default Main
