c82426a31ce034eb8bc217d002849c6c7f8890db
Jessybr <jessicabuenoramos5@gmail.com>
2026-06-23 15:09:27 -0300

    feat: create aboutMe component

---
+++ src/components/aboutMe.tsx
@@ -0,0 +1,12 @@
+function AboutMe() {
+    return (
+        <>
+        <div id="sobre" className="cont_about">
+            <h3 className="topic_title">Sobre mim</h3>
+            <p>Sou uma Desenvolvedora de Software com experiência em projetos utilizando PHP, ReactJS, Bootstrap, Java, banco de dados relacionais, além de um design responsível. Atualmente faço Análise e Desenvolvimento de Sistemas no IFSP e já participei de vários projetos nesse meio tempo. Sou eficaz em equipes, comunicativa e emocionalmente inteligente, promovendo um ambiente positivo. Minha flexibilidade e motivação impulsionam minha busca constante por novos desafios e conhecimentos.</p>
+        </div>
+        </>
+    )
+}
+
+export default AboutMe
