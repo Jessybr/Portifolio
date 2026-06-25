3c48e31356b1bcb9dca0f1a55435d6a7367c4bd9
Jessybr <jessicabuenoramos5@gmail.com>
2026-06-25 17:33:27 -0300

    feat: create useState variables to display Login component

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import Navigation from './components/navigation.tsx'
 import Perfil from './components/perfil.tsx'
 import './style/style.css'
 import Login from './components/login.tsx'
+import { useState } from 'react'
 
 function App() {
+    const [displayFormLogin, setDisplayFormLogin] = useState(false)
 
   return (
     <>
         <Login 
+            displayFormLogin={displayFormLogin}
+            setDisplayFormLogin={setDisplayFormLogin}/>
         <Navigation />
         <Perfil 
+            setDisplayFormLogin={setDisplayFormLogin}/>
         <Main />
     </>
   )
