b85b81eb6d6d8e604713d5569f33a8248a1ca1ba
Jessybr <jessicabuenoramos5@gmail.com>
2026-06-25 17:32:24 -0300

    feat: add login and Perfil components

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import Main from './components/main.tsx'
 import Navigation from './components/navigation.tsx'
 import Perfil from './components/perfil.tsx'
 import './style/style.css'
-import './script/script.js'
+import Login from './components/login.tsx'
 
 function App() {
 
   return (
     <>
-      <Navigation />
-      <Perfil />
-      <Main />
-      
+        <Login 
+        <Navigation />
+        <Perfil 
+        <Main />
     </>
   )
 }
