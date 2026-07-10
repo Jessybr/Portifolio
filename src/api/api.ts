6f7173e7a52ad728025dc1c59c752dbba2486da0
Jessybr <jessicabuenoramos5@gmail.com>
2026-07-10 17:19:36 -0300

    refactor: edit baseURL to localhost to tests

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://portfolio-api-latest-ydsx.onrender.com",
+  //baseURL: "https://portfolio-api-latest-ydsx.onrender.com"
+  baseURL: "http://localhost:3000"
 });
 
 api.interceptors.request.use(
