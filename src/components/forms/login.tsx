cbc652a87b4cc56e7d9f5387e776bf8102c8156d
Jessybr <jessicabuenoramos5@gmail.com>
2026-07-10 16:57:34 -0300

    feat: add function to login

--- src/components/forms/login.tsx
+++ src/components/forms/login.tsx
@@ -1,17 +1,44 @@
+import { useState } from "react";
+import { login } from "../../api/authApi"
+
 interface LoginProps {
     displayFormLogin: boolean
     setDisplayFormLogin: React.Dispatch<React.SetStateAction<boolean>>
+    setLoginIn: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-function Login({ displayFormLogin, setDisplayFormLogin }: LoginProps) {
+function Login({ displayFormLogin, setDisplayFormLogin, setLoginIn }: LoginProps) {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    async function handleSubmit(event: React.SubmitEvent<HTMLFormElement>) {
+        event.preventDefault()
+
+        try {
+            const response = await login({
+                username,
+                password,
+            })
+
+            localStorage.setItem(
+                "token",
+                response.data.token
+            )
+
+            setLoginIn(true)
+        } catch (error) {
+            console.error("Erro ao fazer login", error)
+        }
+    }
+
     return (
         <>
         <div className={displayFormLogin? "fixed cont_login" : "dispNone cont_login"}>
             <span className="closeSpanBlack" onClick={() => setDisplayFormLogin(false)}>X</span>
             <h2>Login</h2>
-            <form action="" method="post">
-                <input type="text" name="email" id="email" placeholder="Email" />
-                <input type="password" name="password" id="password" placeholder="Senha" />
+            <form onSubmit={handleSubmit} method="post">
+                <input type="text" name="email" id="email" placeholder="Email" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                <input type="password" name="password" id="password" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)}/>
                 <button type="submit">Entrar</button>
             </form>
         </div>
