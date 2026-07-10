f4f8a707847a89b5fb79791688c3657110438e3e
Jessybr <jessicabuenoramos5@gmail.com>
2026-07-10 17:18:56 -0300

    feat: create button and function to logout

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,4 +1,14 @@
-function Head() {
+interface HeadProps {
+    loginIn: boolean
+    setLoginIn: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+function Head({ loginIn, setLoginIn }: HeadProps) {
+    function Logout() {
+        localStorage.removeItem("token")
+        setLoginIn(false)
+    }
+
     return (
         <>
         <nav>
@@ -7,6 +17,7 @@ function Head() {
                 <h3><a href="#proje">Projetos</a></h3>
                 <h3><a href="#sobre">Sobre</a></h3>
                 <h3><a href="#contat">Contato</a></h3>
+                <h3><button onClick={Logout} className={loginIn? "dispBlock" : "dispNone"}>Sair</button></h3>
             </div>
         </nav>
         </>
