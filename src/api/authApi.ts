a7bbb4fc639871e0efa43c543da004d0ec73f11b
Jessybr <jessicabuenoramos5@gmail.com>
2026-07-10 16:59:43 -0300

    fix: fix interface and route

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,21 +1,25 @@
 import api from "./api";
 
 interface LoginRequest {
-  email: string;
-  password: string;
+    username: string
+    password: string
 }
 
 interface LoginResponse {
-  token: string;
+    status: number
+    success: boolean
+    message: string
+    data: {
+        token: string
+    }
 }
 
 
 export async function login(
   data: LoginRequest
 ): Promise<LoginResponse> {
-
   const response = await api.post<LoginResponse>(
-    "/auth/login",
+    "/login",
     data
   );
 
