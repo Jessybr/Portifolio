e6e0c632bf535b6298ecbe3929b4b27be47f985f
Jessybr <jessicabuenoramos5@gmail.com>
2026-07-31 19:32:42 -0300

    feat: add getProjectById function

--- src/api/projetosApi.ts
+++ src/api/projetosApi.ts
@@ -1,6 +1,35 @@
 import api from "./api";
 
+interface ProjectData {
+    id: number
+    nome: string
+    descricao: string
+    ativo: boolean
+    videoSrc: string
+    videoPublicId: string
+    imagemSrc: string
+    imagemPublicId: string
+    deployUrl: string
+    githubUrl: string
+    tecnologias: [
+        {
+            tecnologia_id: number
+            projeto_id: number
+            tecnologia: {
+                id: number
+                nome: string
+                iconeSrc: string
+            }
+        }
+    ],
+}
+
 export async function getProjects() {
     const response = await api.get("/project/active")
     return response
+}
+
+export async function getProjectById(id: number): Promise<ProjectData> {
+    const response = await api.get(`/project/${id}`)
+    return response.data.data.project
 }
