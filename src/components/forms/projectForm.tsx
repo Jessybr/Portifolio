e79ed5284ea1a97948a774c8ac575ce9118b144d
Jessybr <jessicabuenoramos5@gmail.com>
2026-07-31 19:31:16 -0300

    feat: create interfaces to project data and formdata

--- src/components/forms/projectForm.tsx
+++ src/components/forms/projectForm.tsx
@@ -3,6 +3,39 @@ interface ProjectFormProps {
     setDisplayProjectForm: React.Dispatch<React.SetStateAction<boolean>>
 }
 
+interface ProjectData {
+    id: number
+    nome: string
+    descricao: string
+    ativo: boolean
+    videoSrc: string
+    videoPublicId: string
+    imagemSrc: string
+    imagemPublicId: string
+    deployUrl: string
+    githubUrl: string
+    tecnologias: Array<{
+        tecnologia_id: number
+        projeto_id: number
+        tecnologia: {
+            id: number
+            nome: string
+            iconeSrc: string
+        }
+    }>
+}
+
+interface ProjectFormData {
+    nome: string
+    descricao: string
+    ativo: string
+    videoSrc: File | null
+    imagemSrc: File | null
+    deployUrl: string
+    githubUrl: string
+    tecnologias: ProjectData["tecnologias"]
+}
+
 function ProjectForm({ displayProjectForm, setDisplayProjectForm }: ProjectFormProps) {
     return (
         <>
