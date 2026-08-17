6fb13d5397dcaad0b2a3195f56020323edc34971
Jessybr <jessicabuenoramos5@gmail.com>
2026-08-17 16:49:30 -0300

    refactor: edit divs and add loading

--- src/components/perfil.tsx
+++ src/components/perfil.tsx
@@ -30,24 +30,29 @@ function Perfil({ displayFormLogin, setDisplayFormLogin, displayPerfilForm, setD
         />
         <header>
             <div className="cont_inicio">
-                <FontAwesomeIcon icon={faPenToSquare} className={loginIn? "faPenToSquare":"faPenToSquare dispNone"} onClick={() => setDisplayPerfilForm(true)}/>
-                <div className="cont_header">
-                    <div className="cont_img">
-                        <img src={perfil?.fotoSrc} alt="Jéssica Bueno Ramos" />
-                    </div>
-                    <div className="cont_title">
-                        <h2>Olá, eu sou a </h2>
-                        <h1 onClick={() => setDisplayFormLogin(true) }>{perfil?.nomeCompleto}</h1>
-                        <h4>{perfil?.breveDescricao}</h4>
-                        {/* <button><a href={perfil?.curriculoSrc} download="Curriculo-Jessica-Bueno-Ramos.pdf" target="_blank">Currículo</a></button> */}
-                        <a href={perfil?.githubUrl} target="_blank">
-                        <FontAwesomeIcon icon={faGithub} size="xl" />
-                        </a>
-                        <a href={perfil?.linkedinUrl} target="_blank">
-                            <FontAwesomeIcon icon={faSquareLinkedin} size="xl" />
-                        </a>
-                    </div>
-                </div>
+                {loading ?
+                    (<p>Carregando perfil...</p>) : 
+                    (<>
+                        <FontAwesomeIcon icon={faPenToSquare} className={loginIn? "faPenToSquare":"faPenToSquare dispNone"} onClick={() => setDisplayPerfilForm(true)}/>
+                        <div className="cont_header">
+                            <div className="cont_img">
+                                <img src={perfil?.fotoSrc} alt="Jéssica Bueno Ramos" />
+                            </div>
+                            <div className="cont_title">
+                                <h2>Olá, eu sou a </h2>
+                                <h1 onClick={() => setDisplayFormLogin(true) }>{perfil?.nomeCompleto}</h1>
+                                <h4>{perfil?.breveDescricao}</h4>
+                                {/* <button><a href={perfil?.curriculoSrc} download="Curriculo-Jessica-Bueno-Ramos.pdf" target="_blank">Currículo</a></button> */}
+                                <a href={perfil?.githubUrl} target="_blank">
+                                <FontAwesomeIcon icon={faGithub} size="xl" />
+                                </a>
+                                <a href={perfil?.linkedinUrl} target="_blank">
+                                    <FontAwesomeIcon icon={faSquareLinkedin} size="xl" />
+                                </a>
+                            </div>
+                        </div>
+                    </>)
+                }
             </div>
         </header>
         </>
