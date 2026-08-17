909f702b12a8fdb7470087240c65ff60f1ff7454
Jessybr <jessicabuenoramos5@gmail.com>
2026-08-17 16:28:55 -0300

    refactor: move forms and add ToastContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
 import Main from './components/main/main.tsx'
 import Navigation from './components/navigation.tsx'
-import Perfil from './components/perfil.tsx'
 import './style/style.css'
-import Login from './components/forms/login.tsx'
 import { useState } from 'react'
-import PerfilForm from './components/forms/perfilForm.tsx'
-import SkillForm from './components/forms/skillForm.tsx'
 import Footer from './components/footer.tsx'
-
-interface PerfilData {
-    id: number
-    nomeCompleto: string
-    linkedinUrl: string
-    githubUrl: string
-    curriculoSrc: string
-    fotoSrc: string
-    breveDescricao: string
-}
+import { Bounce, ToastContainer } from 'react-toastify'
 
 function App() {
     const [loginIn, setLoginIn] = useState<boolean>(() => {
@@ -27,39 +14,26 @@ function App() {
     const [displayPerfilForm, setDisplayPerfilForm] = useState(false)
     const [displaySkillForm, setDisplaySkillForm] = useState(false)
     const [displayProjectForm, setDisplayProjectForm] = useState(false)
-    const [perfil, setPerfil] = useState<PerfilData | null>(null)
     const translucid = displayFormLogin || displayPerfilForm || displaySkillForm || displayProjectForm
 
   return (
     <>
         <div className={translucid? "background_translucid":"dispNone"}></div>
-        <Login 
-            displayFormLogin={displayFormLogin}
-            setDisplayFormLogin={setDisplayFormLogin}
-            setLoginIn={setLoginIn}/>
-        <PerfilForm 
-            displayPerfilForm={displayPerfilForm}
-            setDisplayPerfilForm={setDisplayPerfilForm}
-            setPerfil={setPerfil}/>
-        <SkillForm 
-            displaySkillForm={displaySkillForm}
-            setDisplaySkillForm={setDisplaySkillForm}/>
+        <ToastContainer/>
         <Navigation
             loginIn={loginIn}
             setLoginIn={setLoginIn}/>
-        <Perfil 
-            setDisplayFormLogin={setDisplayFormLogin}
-            displayPerfilForm={displayPerfilForm}
-            setDisplayPerfilForm={setDisplayPerfilForm}
-            loginIn={loginIn}
-            perfil={perfil}
-            setPerfil={setPerfil}/>
         <Main 
+            displaySkillForm={displaySkillForm}
             setDisplaySkillForm={setDisplaySkillForm}
             setDisplayProjectForm={setDisplayProjectForm}
             loginIn={loginIn}
             displayProjectForm={displayProjectForm}
-            />
+            displayFormLogin={displayFormLogin}
+            setDisplayFormLogin={setDisplayFormLogin}
+            displayPerfilForm={displayPerfilForm}
+            setDisplayPerfilForm={setDisplayPerfilForm}
+            setLoginIn={setLoginIn}/>
         <Footer />
     </>
   )
