d2b03cd0e2892b75fe5bd0d8e68010b4294a1d40
Jessybr <jessicabuenoramos5@gmail.com>
2026-08-18 15:59:27 -0300

    feat: add updatePerfil function

--- src/utils/usePerfil.ts
+++ src/utils/usePerfil.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { getPerfil } from "../api/perfilApi"
+import { patchPerfil } from "../api/admin/perfilAdminApi"
 
 interface PerfilData {
     id?: number
@@ -27,13 +28,34 @@ interface PerfilRequest {
 }
 
 function usePerfil() {
-    const [perfil, setPerfil] = useState<PerfilData | null>(null)
+    const [perfil, setPerfil] = useState<PerfilData>()
     const [loading, setLoading] = useState(true)
 
     async function loadPerfil() {
         const response = await getPerfil()
         setPerfil(response.data.data.perfil)
         setLoading(false)
+    }
+
+    async function updatePerfil(formData: PerfilRequest) {
+        const data = new FormData()
+        data.append('nomeCompleto', formData.nomeCompleto)
+        data.append('email', formData.email)
+        data.append('celular', formData.celular)
+        data.append('linkedinUrl', formData.linkedinUrl)
+        data.append('githubUrl', formData.githubUrl)
+        data.append('breveDescricao', formData.breveDescricao)
+        data.append('sobreMim', formData.sobreMim)
+
+        if (formData.pdf) {
+            data.append('pdf', formData.pdf);
+        }
+        if (formData.imagem) {
+            data.append('imagem', formData.imagem);
+        }
+
+        await patchPerfil(data)
+        loadPerfil()
     }
 
     useEffect(() => {
@@ -43,7 +65,8 @@ function usePerfil() {
     return {
         loading, 
         perfil,
-        loadPerfil
+        loadPerfil,
+        updatePerfil
     }
 
 }
