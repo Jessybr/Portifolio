33855d11070ea5219efd2e821ecb5727af3af3d8
Jessybr <jessicabuenoramos5@gmail.com>
2026-08-19 18:11:51 -0300

    refactor: add file rules to validation types

---
+++ src/utils/fileRules.ts
@@ -0,0 +1,18 @@
+export const FILE_RULES = {
+    image: [
+        'image/jpeg',
+        'image/png',
+        'image/webp',
+        'image/gif'
+    ],
+
+    video: [
+        'video/mp4',
+        'video/webm',
+        'video/quicktime'
+    ],
+
+    pdf: [
+        'application/pdf'
+    ]
+}
